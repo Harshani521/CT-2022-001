b25c05aacd0c1bca8f876276717c04b41ade6e56
Harshani Ranasingha <ranasinghaharshani0521@gmail.com>
2026-04-26 23:19:10 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,14 @@
-package Q8;
-
-import java.util.Scanner;
+package Q1;
+import javax.swing.JFrame;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
+        JFrame frame = new JFrame();
 
-        System.out.print("Enter a sentence: ");
-        String sentence = sc.nextLine();
-
-        int index = sentence.indexOf("!");
-
-        String part1 = sentence.substring(0, index);
-        String part2 = sentence.substring(index + 1);
-        part2 = part2.replace(",", "");
-        System.out.println(part1.trim());
-        System.out.println(part2.trim());
+        frame.setTitle("Welcome to Java");
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
